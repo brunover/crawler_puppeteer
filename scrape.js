31e564097c81f1bae88f4cfd349b2eda27f4bfa6
Bruno Leandro de Lima <brunoleandrodelima@gmail.com>
2018-03-22 02:15:38 -0300

    Updated scrape.js to stop only when after geting articles from the last 30 days

--- scrape.js
+++ scrape.js
@@ -1,50 +1,127 @@
+/**
+ * @name ScrapeEmpiricusNews *
+ * @desc Scrapes 'empiricus.com.br' for articles and creates JSON with the ones from the last 30 days *
+ * @author Bruno Leandro de Lima
+ */
 const puppeteer = require('puppeteer')
 const jsonfile = require('jsonfile')
-// const moment = require('moment')
+const moment = require('moment')
+
+const months = 'janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro'.split('_')
+
+// Transform the date string in the article to a JS Date Object
+const convertEmpiricusDate = (dateStr) => {
+  let dateArr = dateStr.substring(dateStr.indexOf('- ') + 2).toLowerCase().replace(',', ' de').split(' de ')
+  let monthInt = months.indexOf(dateArr[1]) + 1
+  let publishDateStr = dateArr[0] + '/' + (monthInt < 10 ? ('0' + monthInt) : ('' + monthInt)) + '/' + dateArr[2]
+  return moment(publishDateStr, 'DD/MM/YYYY').toDate()
+}
 
 const scrape = async () => {
-  // Get the last 30 days to know when to stop searching
-  // const back30Days = moment().subtract(30, 'days')
+  // If it reaches a article older than 30 days, stops scraping
+  let keepScraping = true
+
+  // Page to scrape
+  let currentPage = 'https://www.empiricus.com.br/conteudo/newsletters/'
+
+  // Get the last 31 days to know when to stop
+  let back30Days = moment().subtract(31, 'days')
+
+  // Array with the newsletters that will be used
+  let newsletters = []
 
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true // Turn to 'false' to see the magic happening!
   })
   const page = await browser.newPage()
 
-  await page.goto('https://www.empiricus.com.br/conteudo/newsletters/', {
-    waitUntil: 'domcontentloaded'
-  })
+  console.log('Starting! Will keep scraping until it finds an article equal or older than (' + moment(back30Days).format('DD/MM/YYYY') + ')')
 
-  const newsletters = await page.evaluate(() => {
-    // Will contain all the scraped news
-    let newsArray = []
+  while (keepScraping) {
+    console.log('Current page: ' + currentPage)
 
-    // Select all news from the page
-    let pageNews = document.querySelectorAll('.list-item')
+    await page.goto(currentPage, {
+      waitUntil: 'domcontentloaded'
+    })
 
-    for (let article of pageNews) {
-      let image = article.querySelector('.list-item--thumb > img').getAttribute('src')
-      let title = article.querySelector('.list-item--title').innerText.trim()
-      let description = article.querySelector('.list-item--description').innerText.trim()
+    let pageInfos = await page.evaluate(() => {
+      // Url of the next page
+      let nextPage = document.querySelector('.next').href
 
-      let publishDate = article.querySelector('.list-item--info').innerText.trim()
-      publishDate = publishDate.substring(publishDate.indexOf('- ') + 2).toLowerCase().replace(',', ' de')
+      // Select all news from the page
+      let pageArticles = document.querySelectorAll('.list-item')
 
-      let url = article.querySelector('[id*="btn_post"]').href
-      let text = ''
+      // Will contain all the scraped news
+      let articlesArray = []
 
-      newsArray.push({
-        image,
-        title,
-        publishDate,
-        description,
-        url,
-        text
+      for (let article of pageArticles) {
+        let publishDate = article.querySelector('.list-item--info').innerText.trim()
+        let title = article.querySelector('.list-item--title').innerText.trim()
+        let description = article.querySelector('.list-item--description').innerText.trim()
+        let url = article.querySelector('[id*="btn_post"]').href
+        let text = ''
+
+        let image = ''
+
+        // Sometimes the articles does not have cover images...
+        if (article.querySelector('.list-item--thumb > img') != null) {
+          image = article.querySelector('.list-item--thumb > img').getAttribute('src')
+        }
+
+        articlesArray.push({
+          image,
+          title,
+          publishDate,
+          description,
+          url,
+          text
+        })
+      }
+
+      return {
+        articlesArray,
+        nextPage
+      }
+    })
+
+    // Url of the next page to scrape
+    currentPage = pageInfos.nextPage
+
+    // Go over the articles to obtain the text and check de date
+    let articlesArray = pageInfos.articlesArray
+
+    for (let article of articlesArray) {
+      // Transform the date string in the article to a JS Date Object
+      let publishDate = convertEmpiricusDate(article.publishDate)
+
+      if (moment(publishDate).isBefore(back30Days)) {
+        console.log('Found article from date (' + moment(publishDate).format('DD/MM/YYYY') + '). Stoping operation.')
+        keepScraping = false
+        break
+      }
+
+      // Change the date in article object to a JS Date Object that can be used anywhere
+      article.publishDate = publishDate
+
+      // Go to the article page
+      await page.goto(article.url, {
+        waitUntil: 'domcontentloaded'
       })
-    }
 
-    return newsArray
-  })
+      let scrapedArticleText = await page.evaluate(() => {
+        return document.querySelector('.article--content').innerText
+      })
+
+      // Add the scraped text into the object
+      article.text = scrapedArticleText
+
+      // Add the article to the group of newsletters that will be returned
+      newsletters.push(article)
+
+      // Go back to the newsletters page
+      await page.goBack()
+    }
+  }
 
   await browser.close()
 
@@ -52,13 +129,13 @@ const scrape = async () => {
   return newsletters
 }
 
-scrape().then((newsletters) => {
+scrape().then(newsletters => {
   // Creates a JSON file with the scraped data
   jsonfile.writeFile('newsletters.json', JSON.stringify(newsletters), function (err) {
     if (err) {
       console.error(err)
     } else {
-      console.log('Finished scraping!')
+      console.log('Scraped artciles: ' + newsletters.length)
     }
   })
 })
