de07e0089700c3f29d32e8e629c01bcc8145197d
Bruno Leandro de Lima <brunoleandrodelima@gmail.com>
2018-03-24 20:13:21 -0300

    Updated headless: false -> headless: true

--- scrape.js
+++ scrape.js
@@ -32,7 +32,7 @@ const scrape = async () => {
   let newsletters = []
 
   const browser = await puppeteer.launch({
-    headless: false // Turn to 'false' to see the magic happening!
+    headless: true // Turn to 'false' to see the magic happening!
   })
 
   const page = await browser.newPage()
